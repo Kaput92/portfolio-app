cb0cf418dd71c72f1c01fbaf7dae4732ac0c1805
Kaput92 <emanuele.caputo92@gmail.com>
2025-02-03 11:28:26 +0100

    fixed dark theme & Added project table interactivity

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 const links = document.getElementsByClassName('link');
 const bulb = document.getElementById('fa-lightbulb');
+const table = document.querySelector('table');
+const anchor = document.querySelectorAll('a');
+const list = document.querySelector('.project-one');
+
+    list.addEventListener('click', () => {
+    table.classList.toggle('tableHide');
+    });
+
+
 
 bulb.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
 
     if (document.body.classList.contains('dark-theme')) {
+
+        table.style.borderColor = 'white';
+
+        for (let a of anchor){
+        a.style.color = 'white'
+    };
+
         for (let link of links) {
         link.style.color = 'white'
-    } 
+        };
 }
     else {
+
+        table.style.borderColor = '';
+
+        for (let a of anchor){
+            a.style.color = ''
+        };
+
         for (let link of links){
-        link.style.color = ''}
+        link.style.color = ''
+        };
     }
 })
