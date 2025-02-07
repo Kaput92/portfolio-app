9206555c9f6b42191b42c78fd405d38db4fb279a
Kaput92 <emanuele.caputo92@gmail.com>
2025-02-07 10:51:29 +0100

    Added image slider, fixed css

--- script.js
+++ script.js
@@ -38,3 +38,71 @@ bulb.addEventListener('click', () => {
         };
     }
 })
+
+/* Image Slider */
+
+const imgUrlsArr = [
+    "Media/photo-1460794418188-1bb7dba2720d.avif",
+    "Media/photo-1731612166799-2fbba4e93c42.avif",
+    "Media/photo-1571171637578-41bc2dd41cd2.avif"
+  ];
+  
+  const articleContainer = document.getElementById("article-container");
+  
+  articleContainer.innerHTML = `<img src="${imgUrlsArr[0]}" class="image" />`;
+  
+  let imgIndex = 0;
+  
+  function previousImg() {
+    if (imgIndex > 0 && imgIndex < imgUrlsArr.length) {
+        imgIndex--;
+    } else {
+        imgIndex = imgUrlsArr.length - 1;
+    }
+
+    changeImage(imgIndex);
+}
+
+function nextImg() {
+    if (imgIndex >= 0 && imgIndex < imgUrlsArr.length - 1) {
+        imgIndex++;
+    } else {
+        imgIndex = 0;
+    }
+
+    changeImage(imgIndex);
+}
+
+function showImage(index) {
+    articleContainer.innerHTML = `<img src="${imgUrlsArr[index]}" class="image visible" />`;
+}
+
+/* Funzione per cambiare immagine con transizione */
+function changeImage(index) {
+    const img = document.createElement('img');
+    img.src = imgUrlsArr[index];
+    img.classList.add('image');
+    
+    // Aggiungi l'immagine al container
+    articleContainer.innerHTML = '';
+    articleContainer.appendChild(img);
+
+    // Ritardo per la transizione (minimo per far partire la transizione)
+    setTimeout(() => {
+        img.classList.add('visible');
+    }, 10);
+}
+
+  /* Image Slider Auto Play */
+
+  function startAutoPlay() {
+    setInterval(() => {
+        nextImg();
+    }, 4000); // Cambia immagine ogni 4 secondi
+}
+
+// Mostra la prima immagine all'avvio
+showImage(imgIndex);
+
+// Avvia l'auto-play
+startAutoPlay();
